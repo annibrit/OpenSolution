581ae97445368a12f00f65f70a2dc0844e0ff66a
Susanna <Susanna@DESKTOP-4QTGI21>
2017-05-12 09:52:19 +0300

    Delete proovisime muuta

--- Logic/OrderClasses/OrderLineViewModel.cs
+++ Logic/OrderClasses/OrderLineViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Open.Archetypes.BaseClasses;
 using Open.Archetypes.OrderClasses;
 
-namespace Open.Logic.OrderLineClasses
+namespace Open.Logic.OrderClasses
 {
     public class OrderLineViewModel 
     {
--- Logic/OrderLineClasses/OrderLinesViewModel.cs
+++ Logic/OrderLineClasses/OrderLinesViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Open.Logic.OrderClasses;
 
 namespace Open.Logic.OrderLineClasses
 {
